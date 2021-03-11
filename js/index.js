5cfc92fe0cfdde3acaaa504120562e091da14b20
ranjabi <16520419@mahasiswa.itb.ac.id>
2021-03-11 20:55:37 +0700

    first deploy

---
+++ js/index.js
@@ -0,0 +1,36 @@
+console.log('haloooooooooooooooooo')
+console.log(10 + " sepeluh")
+
+// variable ditinggalkan
+// let a = 7 
+// a = 3
+// console.log(a) 
+
+// OBJECT
+const hardy = {
+    age: 35,
+    nim: 18218004,
+}
+// nyimpen informasi yang berhubungan dengan variable
+
+hardy.age = 20
+console.log(hardy.age)
+
+const bahanMasak = ['apel', 'garam', 'kecap']
+
+
+console.log(bahanMasak)
+
+// let h = (a > 5) ? 10 : 15
+// console.log(h)
+
+// let a = 0
+// while(a < 10) {
+//     console.log(a)
+//     a++
+// }
+
+const a = ['apel', 'anggur', 'amer']
+for(const val in a) {
+    console.log(val)
+}
